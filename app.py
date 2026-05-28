fc9b34840c34f573e93004dd7ad5ea653c7ddea2
GL-Samia <ssaidhassani@galerieslafayette.com>
2026-05-28 16:25:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -252,6 +252,7 @@ if emails_finaux:
 
     #  BOUTONS D'ACTION 
     if st.button("🚀 ENVOYER L'ALERTE", type="primary", use_container_width=True):
+        if st.button("🚀 ENVOYER L'ALERTE", type="primary", use_container_width=True):
     if not mail_cible:
         st.error("Veuillez choisir au moins un destinataire.")
     else:
@@ -259,8 +260,8 @@ if emails_finaux:
             msg = EmailMessage()
             msg['Subject'] = sujet_mail
             msg['From'] = "My Data <mydata@galerieslafayette.com>"
-            msg['To'] = st.secrets["EMAIL_EXPEDITEUR"] # Sécurité : Toi en principal
-            msg['Bcc'] = mail_cible # La liste en caché
+            msg['To'] = st.secrets["EMAIL_EXPEDITEUR"] # Sécurité : Toi
+            msg['Bcc'] = mail_cible # Les autres en caché
             msg.add_alternative(html_mail, subtype='html')
             
             with smtplib.SMTP("smtp.gmail.com", 587) as server:
@@ -268,8 +269,7 @@ if emails_finaux:
                 server.login(st.secrets["EMAIL_EXPEDITEUR"], st.secrets["PASSWORD"])
                 server.send_message(msg)
             
-            
-            st.success(f"✅ Alerte envoyée avec succès !")
+            st.success("✅ Alerte envoyée avec succès !")
             st.balloons()
         except Exception as e:
             st.error(f"Erreur : {e}")
